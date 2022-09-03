ce2afb8cff54c15bc6536de83b97cc8c828b85c2
Zoltán Kasza <zoltan.kasza@ibello.hu>
2022-09-03 18:14:50 +0200

    add: distinct solutions

--- src/test/java/com/amigoscode/examples/DistinctAndSets.java
+++ src/test/java/com/amigoscode/examples/DistinctAndSets.java
@@ -3,6 +3,7 @@ package com.amigoscode.examples;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -14,10 +15,19 @@ public class DistinctAndSets {
     @Test
     public void distinct() throws Exception {
         List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+        List<Integer> distinct = numbers.stream().distinct().collect(Collectors.toList());
+        assertThat(distinct).hasSize(9);
+        System.out.println(distinct);
     }
 
     @Test
     public void distinctWithSet() throws Exception {
         List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+        Set<Integer> distinct = numbers.stream().collect(Collectors.toSet());
+        assertThat(distinct).hasSize(9);
+        System.out.println(distinct);
+        Set<Integer> distinct2 = new HashSet<>(numbers);
+        assertThat(distinct2).hasSize(9);
+        System.out.println(distinct2);
     }
 }
